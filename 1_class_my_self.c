102cc5984a67955dd841509b739e9fe1e7bc5e14
Vansh gupta <mvkmohitgupta@gmail.com>
2026-06-07 12:27:29 +0530

    Create 1_class_my_self.c

---
+++ 1_class_my_self.c
@@ -0,0 +1,15 @@
+include <stdio.h>
+
+int main(void)
+{
+    printf("my name is vansh \n");
+    printf("I am a boy\n");
+    printf("my hobby is playing 🏏🏏\n");
+    printf("my best friend is parth\n");
+    printf("I like unique thing\n");
+    printf("I read in 7 class\n");
+    printf("my favourite colour is blue\n");
+    printf("I like momos\n");
+    printf("I like travelling\n");
+    printf("I have unique thing\n");
+}    
