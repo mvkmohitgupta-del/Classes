e93b3159f667de87a79c5a51020d7a10c28d09d3
Vansh gupta <mvkmohitgupta@gmail.com>
2026-06-20 12:18:24 +0530

    Create 7_class_input.c

---
+++ 7_class_input.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+    //declaring an int variable named num with no value
+    int num;
+    // printing input line 
+    printf("enter the number: ");
+    // taking input for num variable
+    scanf("%i", &num);
+    // printing value of num variable
+    printf("number is: %d\n", num);
+}
